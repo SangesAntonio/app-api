81cf87b806c772017bb49583fc9945c2a5fd6dc8
SangesAntonio <sanges65@gmail.com>
2025-04-08 08:49:43 +0200

    app port

--- app.py
+++ app.py
@@ -82,4 +82,5 @@ def cluster_clienti():
         return jsonify({"errore": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
